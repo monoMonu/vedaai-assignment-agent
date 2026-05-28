d5f0fb526e804a3c75d7a3b297573644c752f502
monoMonu <ms88006218@gmail.com>
2026-05-28 19:30:08 +0530

    add better button to go to dashboard

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,13 @@
+import { ExternalLinkIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col gap-8 justify-center items-center min-h-screen">
       landing Page
-      <Link href="/dashboard">
-        Dashboard
+      <Link href="/dashboard/assignments" className="bg-primary text-primary-foreground px-8 py-3 rounded-full flex gap-2">
+        Dashboard <ExternalLinkIcon />
       </Link>
-    </>
+    </div>
   );
 }
